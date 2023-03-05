de258a7af04073b5a9603d6422240515ad4dc893
Matthew Sasakamoose <matt.sasakamoose@gmail.com>
2023-03-04 21:02:10 -0700

    more stuff

--- API/generate.py
+++ API/generate.py
@@ -1,5 +1,4 @@
-file = open("voltages.txt", 'w')
-for i in range (0, 2**8):
-    vout = i * 250 / (2**8)
-    file.write("{0:b}".format(i).zfill(8) + " = " + vout.__str__() + '\n')
-file.close()
+with open("voltages.txt", 'w') as file:
+    for i in range (0, 2**8):
+        vout = i * 250 / (2**8)
+        file.write("{0:b}".format(i).zfill(8) + " = " + vout.__str__() + '\n')
