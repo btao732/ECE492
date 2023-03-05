4c068d8e748e9d06559b1101a963e1f3b18f9e9f
Matthew Sasakamoose <matt.sasakamoose@gmail.com>
2023-03-05 12:07:35 -0700

    comments

--- API/sparc.hpp
+++ API/sparc.hpp
@@ -8,16 +8,41 @@
 #include <fstream>
 #include <map>
 
+/*
+Class for initializing and controlling SPARC arrays.
+*/
 class SPARC {
 private:
+    /*
+    Lookup Table for converting float voltages to 
+    an 8 bit representation.
+    */
     std::map<float, uint8_t> voltageMap; 
 
-
-
 public:
+    /*
+    Default constructor uses file "voltages.txt" to 
+    generate voltageMap lookup table.
+    */
     SPARC();
+
+    /*
+    Overrided constructor uses filename parameter to
+    generate voltageMap lookup table, file must be of
+    format '8_bit_representation mapped_voltage\n'.
+    */
     SPARC(char *filename);
+
+    /*
+    Defaulted destructor.
+    */
     ~SPARC() = default;
+    
+    /*
+    Function takes float and converts to 8-bit 
+    representation that is closest to the 
+    desired voltage.
+    */
     uint8_t encodeVoltage(float voltage);
 };
 
