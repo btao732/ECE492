6968145556985da870096ec92a8e72342d984dba
sasakamoo <91626250+sasakamoo@users.noreply.github.com>
2023-04-03 21:30:15 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -14,14 +14,15 @@
  */
 #define UART_DEVICE_ID                  XPAR_XUARTPS_0_DEVICE_ID
 #define GPIO_DEVICE_ID 					XPAR_GPIO_0_DEVICE_ID
-#define BUFFER_SIZE		1000
+#define BUFFER_SIZE						10
+#define UART_DELAY_MICROSECONDS			10000
+#define CLOCK_DELAY_MICROSECONDS 		200000
 
 int UartPsSetUp(u16 DeviceId);
 int GpioSetup(u16 DeviceId);
 
 XUartPs uart;		/* The instance of the UART Driver */
-XGpio gpio;
-
+XGpio gpio;			/* The instance of the GPIO Driver */
 
 // Convert u8 buffer to u32 data
 u32 bufferToUnsigned32(u8* buffer, u32 index) {
@@ -46,16 +47,19 @@ u16* loadChannelDataPISO(u8* buffer, u32 numberOfChannels, u32 numberOfSamples, 
 // Max of 30 channels
 void sendChannelData(u8 channelData[], u32 numberOfChannels, u32 samplesPerChannel) {
 	for (int sample = 0; sample < samplesPerChannel; sample++) {
-		for (int byte = 0; byte < 2; byte++) {
-			for (int bit = 7; bit >= 0; bit--) {
+		for (int byte = 1; byte >= 0; byte--) {
+			for (int bit = 0; bit < 8; bit++) {
 				u32 outputData = 0; // Datatype determines max number of channels
 				u8 dataMask = 1 << bit;
 				for (int channel = 0; channel < numberOfChannels; channel++) {
-					u8 rawData = channelData[channel * samplesPerChannel + sample + byte];
-					outputData += ((rawData & dataMask) >> bit) << channel; // Fixed
+					u8 rawData = channelData[channel * samplesPerChannel * 2 + sample * 2 + byte];
+					outputData |= ((rawData & dataMask) >> bit) << (channel + 2);
 				}
 				XGpio_DiscreteWrite(&gpio, 1, outputData);
-				usleep(100000);
+				usleep(CLOCK_DELAY_MICROSECONDS);
+				outputData |= 1 | ((((!byte) + bit) >> 3) << 1); // Clock and Latch signals
+				XGpio_DiscreteWrite(&gpio, 1, outputData);
+				usleep(CLOCK_DELAY_MICROSECONDS);
 			}
 		}
 	}
@@ -80,49 +84,56 @@ int main(void)
 
 	XGpio_SetDataDirection(&gpio, 1, 0xF);
 
-	// Get Number of channel and samples per channel over UART
-	u32 byteSum = 0;
-	while (byteSum < 8) {
-		u32 receivedBytes = XUartPs_Recv(&uart, buffer, 8 - byteSum);
-		for (int i = 0; i < receivedBytes; i++) {
-			metaData[byteSum + i] = buffer[i];
-		}
-		byteSum += receivedBytes;
-	}
+	xil_printf("SPARC Initialized\r\n");
 
-	u32 numberOfChannels = bufferToUnsigned32(metaData, 0);
-	u32 numberOfSamples = bufferToUnsigned32(metaData, 4);
-
-	xil_printf("Channels: %u\r\n", numberOfChannels);
-	xil_printf("Samples per channel: %u\r\n", numberOfSamples);
-
-	u32 totalDataBytes = numberOfChannels * numberOfSamples * 2;
-
-	// So we don't blow up the fpga by asking for too much memory
-	if (totalDataBytes < 10000) {
-		data = (u8*) malloc(totalDataBytes); // 2 Bytes per sample
-		byteSum = 0;
-		while (byteSum < totalDataBytes) {
-			u32 receivedBytes = XUartPs_Recv(&uart, buffer, totalDataBytes - byteSum);
+	while (1) {
+		// Get Number of channel and samples per channel over UART
+		u32 byteSum = 0;
+		while (byteSum < 8) {
+			u32 receivedBytes = XUartPs_Recv(&uart, buffer, 8 - byteSum);
 			for (int i = 0; i < receivedBytes; i++) {
-				data[byteSum + i] = buffer[i];
+				metaData[byteSum + i] = buffer[i];
 			}
 			byteSum += receivedBytes;
+			usleep(UART_DELAY_MICROSECONDS);
 		}
-	} else {
-		xil_printf("Error - Data Overflow\r\n");
+
+		u32 numberOfChannels = bufferToUnsigned32(metaData, 0);
+		u32 numberOfSamples = bufferToUnsigned32(metaData, 4);
+
+		xil_printf("Channels: %u\r\n", numberOfChannels);
+		xil_printf("Samples per channel: %u\r\n", numberOfSamples);
+
+		u32 totalDataBytes = numberOfChannels * numberOfSamples * 2;
+
+		// So we don't blow up the fpga by asking for too much memory
+		if (totalDataBytes < 10000) {
+			data = (u8*) malloc(totalDataBytes); // 2 Bytes per sample
+			byteSum = 0;
+			while (byteSum < totalDataBytes) {
+				u32 receivedBytes = XUartPs_Recv(&uart, buffer, totalDataBytes - byteSum);
+				for (int i = 0; i < receivedBytes; i++) {
+					data[byteSum + i] = buffer[i];
+				}
+				byteSum += receivedBytes;
+				usleep(UART_DELAY_MICROSECONDS);
+			}
+
+			// Print channel data
+			for (int i = 0; i < totalDataBytes; i++) {
+				xil_printf("%x ", data[i]);
+			}
+			xil_printf("\r\n");
+
+			// Output to PMOD
+			sendChannelData(data, numberOfChannels, numberOfSamples);
+			free(data);
+
+		} else {
+			xil_printf("Error - Data Overflow\r\n");
+		}
 	}
 
-	// Print channel data
-	for (int i = 0; i < totalDataBytes; i++) {
-		xil_printf("%x ", data[i]);
-	}
-	xil_printf("\r\n");
-
-	// Output to PMOD
-	sendChannelData(data, numberOfChannels, numberOfSamples);
-
-	free(data);
 	return 0;
 }
 
@@ -147,7 +158,6 @@ int UartPsSetUp(u16 DeviceId)
 	}
 
 	XUartPs_SetBaudRate(&uart, 115200);
-
 
 	return Status;
 }
