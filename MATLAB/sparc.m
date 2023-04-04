e640b4a90ed9df16057f412347ddf366f7ab47be
sasakamoo <91626250+sasakamoo@users.noreply.github.com>
2023-04-03 21:32:58 -0600

    Add files via upload

--- MATLAB/sparc.m
+++ MATLAB/sparc.m
@@ -1,12 +1,16 @@
 classdef sparc
-    %SPARC Summary of this class goes here
-    %   Detailed explanation goes here
-    
+    % Class for initializing and controlling SPARC arrays.
+
     properties (Access = private)
+        % Lookup Table for converting float voltages to 
+        % an 8 bit representation.
         voltageMap
     end
 
     methods (Access = public, Static)
+        % Function developed by Randy Palamar to generate
+        % appropriate 16 bit value from the 8 bit value,
+        % dependant on hardware architecture.
         function r = map_8_to_16(b)
             arguments (Input)
                 b uint8 {mustBeNumeric}
@@ -23,11 +27,48 @@ classdef sparc
                 end
             end
         end
+
+        % Function to generate calibration voltages for
+        % all input combinations
+        function createCalibrationVoltages(voltageIn)
+            if nargin < 1
+                voltageIn = 250.0;
+            end
+            outputfile = fopen('voltages.txt', 'w');
+            for i = 0:2^8-1
+                fprintf(outputfile, "%d %f\n", i, i * voltageIn / (2^8));
+            end
+            fclose(outputfile);
+        end
+        
+        % Helper function to send waveform binary 
+        % over first available serial port
+        function sendWaveform()
+            freeports = serialportlist("available");
+            if isempty(freeports)
+                disp("No serial port found");
+                return
+            end
+            device = serialport(freeports(1), 115200);
+            inputfile = fopen("waveform.bin");
+            [data, size] = fread(inputfile, [1 Inf], 'uint8');
+            for i = 1:size
+                write(device, data(i), 'uint8');
+            end
+            fclose(inputfile);
+            delete(device);
+        end
     end
     
     methods (Access = public)
-        function obj = sparc
+        % Constructor uses file "voltages.txt" to 
+        % generate voltageMap lookup table.
+        function obj = sparc(maxVoltage)
             obj.voltageMap = containers.Map('KeyType', 'single', 'ValueType', 'uint8');
+            if nargin < 1
+                maxVoltage = 250;
+            end
+            sparc.createCalibrationVoltages(maxVoltage);
             inputfile = fopen('voltages.txt', 'r');
             voltages = fscanf(inputfile, '%d %f\n', [2 Inf]);
             for i = 1:256
@@ -37,24 +78,30 @@ classdef sparc
             disp('Initialized SPARC');
             fclose(inputfile);
         end
-
-        function uploadChannelWaveform(obj, A, n, l)
+        
+        % Upload voltage waveform for n channels to SPARC 
+        % system, takes an n*l matrix of floats.
+        function generateChannelWaveform(obj, A)
             arguments (Input)
                 obj sparc
                 A (:,:) single
-                n {mustBePositive}
-                l {mustBePositive}
             end
+            [n, l] = size(A);
             outputfile = fopen('waveform.bin','w');
+            fwrite(outputfile, n, "uint32", 0, 'b'); % Write number of channels first
+            fwrite(outputfile, l, "uint32", 0, 'b'); % Write samples per channel second
             for i = 1:n
                 for j = 1:l
                     r = obj.map_8_to_16(obj.encodeVoltage(A(i,j)));
                     fwrite(outputfile, r,'uint16',0,'b');
                 end
-            end 
+            end
             fclose(outputfile);
         end
-
+            
+        % Function takes float and converts to 8-bit 
+        % representation that is closest to the 
+        % desired voltage.
         function val = encodeVoltage(obj, desiredVoltage)
             arguments (Input)
                 obj sparc
@@ -75,4 +122,3 @@ classdef sparc
         end
     end
 end
-
